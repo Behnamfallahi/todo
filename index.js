c6dd0a1840c06ce8ea8cbbb70a025ee612b9f8fd
behnam falahi <falahib86@gmail.com>
2025-08-18 16:10:41 +0330

    delete_selector_search

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 let list = document.querySelector("#to_do_list");
  let input =  document.querySelector("#input_add_todo input");
- let search = document.querySelector("#input_search");
  let add = document.querySelector("#input_add_todo button");
 
