92ca2320227106527c2484b4b1b3ea1661cae0bb
behnam falahi <falahib86@gmail.com>
2025-08-18 16:09:36 +0330

    add_selector

---
+++ index.js
@@ -0,0 +1,5 @@
+let list = document.querySelector("#to_do_list");
+ let input =  document.querySelector("#input_add_todo input");
+ let search = document.querySelector("#input_search");
+ let add = document.querySelector("#input_add_todo button");
+
